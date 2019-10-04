800b1a090543696a2c9e34d0d50faf25ec468fbd
marciano <mcnvrodrigues@gmail.com>
2019-10-03 22:31:59 -0300

    valida input

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-class game{
+class Game{
     constructor(){
         this.keySelected = 0;
         this.digitSelected = 0;
@@ -21,7 +21,7 @@ class game{
     }
 }
 
-class block{
+class Block{
     constructor(digits){
         this.digits = digits;
     }
@@ -31,58 +31,93 @@ class block{
     }
 
     set(row, column, digit){
-        digits[row][column] = digit;
+        this.digits[row][column] = digit;
     }
 }
 
-class grid{
+class Grid{
     constructor(blocks){
         this.blocks = blocks;        
-    }
+    }    
 
-    
-
-    get(){
+    getBlocks(){
         return this.blocks;
     }
 
-    getBlockByIndex(index){
-        switch(index){
-            case 0:
-                return this.blocks[0][0];
-                break;
-            case 1:
-                return this.blocks[0][1];
-                break;
-            case 2:
-                return this.blocks[0][2];
-                break;
-            case 3:
-                return this.blocks[1][0];
-                break;
-            case 4:
-                return this.blocks[1][1];
-                break;
-            case 5:
-                return this.blocks[1][2];
-                break;
-            case 6:
-                return this.blocks[2][0];
-                break;
-            case 7:
-                return this.blocks[2][1];
-                break;
-            case 8:
-                return this.blocks[2][2];
-                break;
-            default:
-                return undefined;
+    getBlockByIndex(index){        
+        return this.blocks[index];        
+    }
+
+    getBlockByIndexToArray(index){ 
+        let block = [];
+        
+        for(let i = 0; i < 3; i += 1){
+            for(let j = 0; j < 3; j += 1){
+                //console.log("err >> ", this.getBlockByIndex(index).getDigits()[i][j]);
+                block.push(this.getBlockByIndex(index).getDigits()[i][j]);
+            }
         }
+        return block;        
     }
 
     updateBlock(index,block){
         blocks[index] = block;
     }
+
+    //retorna os elementos da linha que o usuário selecionou
+    getNumbersRow(bl, r){
+
+        let row = [];
+        
+        if(bl < 3){
+            for(let i = 0; i < 3; i++){                
+                for(let j = 0; j < 3; j++){
+                    row.push(this.getBlockByIndex(i).getDigits()[r][j]);
+                }                
+            }
+        }else if(bl < 6){
+            for(let i = 3; i < 6; i++){                
+                for(let j = 0; j < 3; j++){
+                    row.push(this.getBlockByIndex(i).getDigits()[r][j]);
+                }                
+            }
+        }else{
+            for(let i = 6; i < 9; i++){                
+                for(let j = 0; j < 3; j++){
+                    row.push(this.getBlockByIndex(i).getDigits()[r][j]);
+                }                
+            }
+        }
+
+        return row;
+    }
+
+    // retorna os elementos da coluna que o usuário selecionou
+    getNumbersColumn(bl, c){
+        let column = [];
+        
+        if(bl === 0 || bl === 3 || bl === 6){
+            for(let i = 0; i <= 6; i += 3){                
+                for(let j = 0; j < 3; j++){
+                    column.push(this.getBlockByIndex(i).getDigits()[j][c]);
+                }                
+            }
+        }else if(bl === 1 || bl === 4 || bl === 7){
+            for(let i = 1; i <= 7; i += 3){                
+                for(let j = 0; j < 3; j++){
+                    column.push(this.getBlockByIndex(i).getDigits()[j][c]);
+                }                
+            }
+        }else{
+            for(let i = 2; i <= 8; i += 3){                
+                for(let j = 0; j < 3; j++){
+                    column.push(this.getBlockByIndex(i).getDigits()[j][c]);
+                }                
+            }
+        }
+
+        return column;
+    }    
 }
 
 let bl_a = [[2, 3, 4], 
@@ -121,38 +156,95 @@ let bl_i = [[6, 7, 8],
             [3, 4, 5],
             [9, 1, 2]];
 
-let blck_a = new block(bl_a);
-let blck_b = new block(bl_b);
-let blck_c = new block(bl_c);
 
-let blck_d = new block(bl_d);
-let blck_e = new block(bl_e);
-let blck_f = new block(bl_f);
+let bl_a_user = [[0, 0, 0], 
+                 [0, 0, 0],
+                 [0, 0, 0]];
 
-let blck_g = new block(bl_g);
-let blck_h = new block(bl_h);
-let blck_i = new block(bl_i);
+let bl_b_user = [[0, 0, 0], 
+                 [0, 0, 0],
+                 [0, 0, 0]];
 
-let gr = [[blck_a, blck_b, blck_c],
-          [blck_d, blck_e, blck_f],
-          [blck_g, blck_h, blck_i]];
+let bl_c_user = [[0, 0, 0], 
+                 [0, 0, 0],
+                 [0, 0, 0]];
 
-let match = new game();
+let bl_d_user = [[0, 0, 0], 
+                 [0, 0, 0],
+                 [0, 0, 0]];
 
-let sudoku = new grid(gr);
+let bl_e_user = [[0, 0, 0], 
+                 [0, 0, 0],
+                 [0, 0, 0]];
+
+let bl_f_user = [[0, 0, 0], 
+                 [0, 0, 0],
+                 [0, 0, 0]];
+
+let bl_g_user = [[0, 0, 0], 
+                 [0, 0, 0],
+                 [0, 0, 0]];
+
+let bl_h_user = [[0, 0, 0], 
+                 [0, 0, 0],
+                 [0, 0, 0]];
+
+let bl_i_user = [[0, 0, 0], 
+                 [0, 0, 0],
+                 [0, 0, 0]];
+
+let blck_a = new Block(bl_a);
+let blck_b = new Block(bl_b);
+let blck_c = new Block(bl_c);
+
+let blck_d = new Block(bl_d);
+let blck_e = new Block(bl_e);
+let blck_f = new Block(bl_f);
+
+let blck_g = new Block(bl_g);
+let blck_h = new Block(bl_h);
+let blck_i = new Block(bl_i);
+
+let blck_a_user = new Block(bl_a_user);
+let blck_b_user = new Block(bl_b_user);
+let blck_c_user = new Block(bl_c_user);
+
+let blck_d_user = new Block(bl_d_user);
+let blck_e_user = new Block(bl_e_user);
+let blck_f_user = new Block(bl_f_user);
+
+let blck_g_user = new Block(bl_g_user);
+let blck_h_user = new Block(bl_h_user);
+let blck_i_user = new Block(bl_i_user);
+
+let gr = [blck_a, blck_b, blck_c,
+          blck_d, blck_e, blck_f,
+          blck_g, blck_h, blck_i
+         ];
+
+let gr_user = [blck_a_user, blck_b_user, blck_c_user,
+               blck_d_user, blck_e_user, blck_f_user,               
+               blck_g_user, blck_h_user, blck_i_user];
+
+let match = new Game();
+
+let solution = new Grid(gr);
+let userSolution = new Grid(gr_user);
+
 
 function digitClicked(e){
-    console.log('Digit clicked: ', e.currentTarget);
+    //console.log('Digit clicked: ', e.currentTarget);
     e.currentTarget.style.backgroundColor = "red";
     match.setDigitSelected(e.currentTarget);
 }
 
 function keyClicked(e){
     //debugger;
-    console.log('Key clicked: ', e.currentTarget.innerHTML);
+    //console.log('Key clicked: ', e.currentTarget.innerHTML);
     match.setKeySelected(e.currentTarget);
+
     if(match.getDigitSelected()){
-        console.log(match.getDigitSelected());
+        //console.log(match.getDigitSelected());
         match.getDigitSelected().style.backgroundColor = "white";
     }else{
         console.log('Digito nao selecionado');
@@ -161,11 +253,41 @@ function keyClicked(e){
     linkKeyDigit();
 }
 
+function checkIfNumberIsValid(number){
+    let numberIsValid = true;
+
+    let key = parseInt(match.getKeySelected().innerText);
+    let digit = match.getDigitSelected();
+
+    let block = parseInt(digit.getAttribute('data-block-index')); //recupera block selecionado pelo usuário
+    let row = digit.getAttribute('data-row'); // recupera linha selecionada pelo usuário
+    let column = digit.getAttribute('data-column'); // recupera coluna selecionada pelo usuário
+        
+    
+    return numberIsValid;
+}
+
+// Verifica se uma posição foi selecionada no grid e, se um numero do teclado foi selecionado
 function linkKeyDigit(){
     if(match.getDigitSelected() !== 0){
         if(match.getKeySelected() !== 0){
-            let key = match.getKeySelected().innerHTML;
+            let key = parseInt(match.getKeySelected().innerText);
             let digit = match.getDigitSelected();
+
+            let block = parseInt(digit.getAttribute('data-block-index')); //recupera block selecionado pelo usuário
+            let row = digit.getAttribute('data-row'); // recupera linha selecionada pelo usuário
+            let column = digit.getAttribute('data-column'); // recupera coluna selecionada pelo usuário
+
+            userSolution.getBlockByIndex(block).set(row, column, key);// atribui valor selecionado pelo usuário no grid
+
+            console.log(`Block ${block}, row ${row} column ${column} key ${key}`);
+
+            console.log("row ", userSolution.getNumbersRow(block,row));
+            console.log("column ", userSolution.getNumbersColumn(block,column));
+            console.log("Block ", userSolution.getBlockByIndex(block));
+            console.log("Block ", userSolution.getBlockByIndexToArray(block));
+
+
             digit.innerHTML = `<div class="num">${key}</div>`;
         }
     }
@@ -173,7 +295,7 @@ function linkKeyDigit(){
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     let html = '';
-    let digit = undefined;
+    let digit, digit_usr = undefined;
 
     for(let bl = 0; bl < 9; bl += 1){    
 
@@ -183,11 +305,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
             for (let column = 0; column < 3; column += 1){
 
-                digit = sudoku.getBlockByIndex(bl).getDigits()[row][column];
+                digit = solution.getBlockByIndex(bl).getDigits()[row][column];      
+                //console.log(userSolution.getBlockByIndex(bl).getDigits().set(row, column, digit));         
                 
                 let showNum = Math.floor(Math.random() * 10);                
 
-                if (showNum < 3){                    
+                if (showNum < 3){        
+                    userSolution.getBlockByIndex(bl).set(row, column, digit);
+        
                     html += `<div class="digit" data-block-index = "${bl}" data-row = "${row}" data-column = "${column}"><div class="num">${digit}</div></div>`;
                 }else{
                     html += `<div class="digit" data-block-index = "${bl}" data-row = "${row}" data-column = "${column}"><div class="num"></div></div>`;
@@ -198,7 +323,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         html += `</div>`;
     }
   
-    
+    //console.table(userSolution);
     let mainGrid = document.querySelector('#main_grid');
   
     if(mainGrid){
